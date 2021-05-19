51428ea902b36082cd223067e05c7b0c56dcf0b1
zoytsa <a.baturskiy@gmail.com>
2021-05-19 23:23:32 +0600

    litle ios issues with vsc

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/aikantoktogonova/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aikantoktogonova/Development/firebase_sample/firebase_sample2/flutter_firebase_auth_app"
+export "FLUTTER_TARGET=/Users/aikantoktogonova/Development/firebase_sample/firebase_sample2/flutter_firebase_auth_app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/aikantoktogonova/Development/firebase_sample/firebase_sample2/flutter_firebase_auth_app/.dart_tool/package_config.json"
